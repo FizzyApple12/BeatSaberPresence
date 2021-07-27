7ca581b62d46688c7271920e6ba8eeebb115f96c
FizzyApple12 <davideroeder@gmail.com>
2021-07-26 23:55:21 -0400

    bumped dependencies and added FizzyUtils integration

--- BeatSaberPresence/Plugin.cs
+++ BeatSaberPresence/Plugin.cs
@@ -9,13 +9,17 @@ using IPALogger = IPA.Logging.Logger;
 namespace BeatSaberPresence {
     [Plugin(RuntimeOptions.DynamicInit)]
     public class Plugin {
-        internal const string Name = "Beat Saber Presence";
+        internal const string Name = "BeatSaberPresence";
+
+        FizzyUtils.UsageTracker.UsageTrackerUser usageTrackerUser;
 
         [Init]
         public Plugin(Conf conf, IPALogger logger, Zenjector zenjector) {
             zenjector.OnApp<PluginInstaller>().WithParameters(logger, conf.Generated<PluginConfig>());
             zenjector.OnGame<PresenceGameInstaller>(false);
             zenjector.OnMenu<PresenceMenuInstaller>();
+
+            usageTrackerUser = FizzyUtils.Utils.usageTracker.AddUser(Name);
         }
 
         [OnEnable, OnDisable]
